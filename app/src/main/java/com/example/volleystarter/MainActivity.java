f0aca99137cf151bf7368b5ce5bb3f1d3ae9ea13
CodeNextSam <52679947+sameetasatya@users.noreply.github.com>
2020-04-18 12:14:07 -0700

    Initial commit

---
+++ app/src/main/java/com/example/volleystarter/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.volleystarter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView plotTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        plotTextView = findViewById(R.id.plot_tv);
+    }
+
+    public void fetchData(View view) {
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://www.omdbapi.com/?apikey=11a900d7&t=Black%20Panther";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try{
+                            JSONObject responseObject = new JSONObject(response);
+                            String responsePiece = responseObject.getString("Plot");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        plotTextView.setText("Response is: " + response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                plotTextView.setText("That didn't work!");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+}
